d56a5ae2f05a0f83b7f435b77b58a3000abf9349
jezia126 <lefkihlefkih123@gmail.com>
2022-01-24 12:46:17 +0100

    first commit

---
+++ src/app/sha/api.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private _http:HttpClient) { }
+  //post get put delete method
+  postEmployee(data:any){
+    return this._http.post<any>("http://localhost:3000/posts",data).pipe(
+      map(
+        (res=>{
+          return res;
+        })
+      )
+    )
+  }
+
+  getEmployee(){
+    return this._http.get<any>("http://localhost:3000/posts").pipe(
+      map(
+        (res=>{
+          return res;
+        })
+      )
+    )
+  }
+
+  putEmployee(data:any,id:number){
+    return this._http.put<any>("http://localhost:3000/posts/"+id,data).pipe(
+      map(
+        (res=>{
+          return res;
+        })
+      )
+    )
+  }
+  deleteEmployee(id:number){
+    return this._http.delete<any>("http://localhost:3000/posts/"+id).pipe(
+      map(
+        (res=>{
+          return res;
+        })
+      )
+    )
+  }
+
+}
